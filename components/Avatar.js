da7e8b42d00ca1e25f73d66bc54893393666b6b8
moahammadApon <mohammadapon11.com>
2023-12-20 00:16:13 +0600

    avator margin control

--- components/Avatar.js
+++ components/Avatar.js
@@ -4,7 +4,7 @@ import Image from "next/image";
 const Avatar = () => {
   return (
     <div className="hidden xl:flex xl:max-w-none">
-      <Image src={'/apon.png'} priority={true} width={737} height={678} alt=""  className="translate-z-0 xl:w-[500px] lg:w-[400px] md:w-[350px]" />
+      <Image src={'/apon.png'} priority={true} width={737} height={678} alt=""  className="translate-z-0 xl:w-[600px] lg:w-[500px] md:w-[400px]" />
     </div>
   );
 };
